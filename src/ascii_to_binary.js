38d7d8a40e6fe385b85c51de75d290cc5282390e
WisW <78122384+winsrewu@users.noreply.github.com>
2025-08-29 19:10:50 +0800

    feat: input feature & enchanced debug

--- src/ascii_to_binary.js
+++ src/ascii_to_binary.js
@@ -14,7 +14,18 @@ function get_should_print(num) {
     return get_corresponding_char(num);
 }
 
+function is_printable(num) {
+    return num >= 32 && num <= 126;
+}
+
+function is_legal(num) {
+    return get_corresponding_char(num) != "\"" && get_corresponding_char(num) != "\\";
+}
+
 module.exports = {
     char_to_num: char_to_num,
-    get_should_print: get_should_print
+    get_should_print: get_should_print,
+    is_printable: is_printable,
+    get_corresponding_char: get_corresponding_char,
+    is_legal: is_legal
 };
