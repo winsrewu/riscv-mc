4b3281f66677cbb406e8164f193008ff39b862a6
WisW <78122384+winsrewu@users.noreply.github.com>
2025-09-06 16:25:47 +0800

    feat: dont remove elf file when build

--- c/build.sh
+++ c/build.sh
@@ -3,7 +3,6 @@ set -e
 riscv32-unknown-elf-gcc -g -O3 -static -specs=nosys.specs -march=rv32i -mabi=ilp32 -T include/link.ld -o app.elf include/vectors.S include/syscalls.c main.c -lc -lgcc
 riscv32-unknown-elf-objcopy -F verilog app.elf app.mem
 
-rm app.elf
 mv app.mem ../python
 cd ../python
 python3 mem_to_mcf.py app.mem
