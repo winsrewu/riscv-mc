0ed0b5adab6e13d7b932174d0c7e346495749678
WisW <78122384+winsrewu@users.noreply.github.com>
2025-09-06 16:46:01 +0800

    perf: use submodule

--- c/build.sh
+++ c/build.sh
@@ -1,6 +1,6 @@
 set -e
 
-riscv32-unknown-elf-gcc -g -O3 -static -specs=nosys.specs -march=rv32i -mabi=ilp32 -T include/link.ld -o app.elf include/vectors.S include/syscalls.c main.c -lc -lgcc
+riscv32-unknown-elf-gcc -g -O3 -static -specs=nosys.specs -march=rv32i -mabi=ilp32 -T ../pyriscv/app/c-common/link.ld -o app.elf ../pyriscv/app/c-common/vectors.S ../pyriscv/app/c-common/syscalls.c main.c -lc -lgcc
 riscv32-unknown-elf-objcopy -F verilog app.elf app.mem
 
 mv app.mem ../python
