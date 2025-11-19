55f2c70b876e6171ece7bdeed7e406fb8c8fe079
WisW <78122384+winsrewu@users.noreply.github.com>
2025-11-19 23:32:30 +0800

    fix: special char issues

--- src/ascii_to_binary.js
+++ src/ascii_to_binary.js
@@ -11,22 +11,19 @@ function get_should_print(num) {
     if (c == 10) return "\\n";
     if (c == 9) return "    ";
     if (c < 32 || c == 126) return "";
+    if (c == 34) return "\\\"";
+    if (c == 39) return "\\'";
+    if (c == 92) return "\\\\";
     if (c > 126) return "?";
     return get_corresponding_char(num);
 }
 
 function is_printable(num) {
-    return num >= 32 && num <= 126;
-}
-
-function is_legal(num) {
-    return get_corresponding_char(num) != "\"" && get_corresponding_char(num) != "\\";
+    return (num >= 32 && num <= 126) || num == 10 || num == 9;
 }
 
 module.exports = {
     char_to_num: char_to_num,
     get_should_print: get_should_print,
-    is_printable: is_printable,
-    get_corresponding_char: get_corresponding_char,
-    is_legal: is_legal
+    is_printable: is_printable
 };
